9046ef614d6f7150a53637d8f321cf801685832b
SntiagoC <cs.sioga@gmail.com>
2022-11-16 17:02:01 -0500

    inicio error

--- Frontend/FrontendEmpresa/src/app/app-routing.module.ts
+++ Frontend/FrontendEmpresa/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'inicio',
+    component:InicioComponent
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/inicio'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/FrontendEmpresa/src/app/app.module.ts
+++ Frontend/FrontendEmpresa/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
 import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { ErrorComponent } from './plantilla/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraNavegacionComponent,
-    PiePaginaComponent
+    PiePaginaComponent,
+    InicioComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
