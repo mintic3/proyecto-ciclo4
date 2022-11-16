38d12ed60d080b441d04b98fa1f1da1cf5ef89b8
SntiagoC <cs.sioga@gmail.com>
2022-11-16 17:12:25 -0500

    Revert "inicio error"
    This reverts commit 9046ef614d6f7150a53637d8f321cf801685832b.

--- Frontend/FrontendEmpresa/src/app/app-routing.module.ts
+++ Frontend/FrontendEmpresa/src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [
-  {
-    path:'inicio',
-    component:InicioComponent
-  },
-  {
-    path:'',
-    pathMatch:'full',
-    redirectTo:'/inicio'
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
