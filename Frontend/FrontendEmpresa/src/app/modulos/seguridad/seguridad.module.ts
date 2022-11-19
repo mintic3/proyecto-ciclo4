40654d2ae5c3a630cec1e7f1271aaa6e5c0a5177
SntiagoC <cs.sioga@gmail.com>
2022-11-18 19:13:48 -0500

    Identificacion

--- Frontend/FrontendEmpresa/src/app/modulos/seguridad/seguridad.module.ts
+++ Frontend/FrontendEmpresa/src/app/modulos/seguridad/seguridad.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { SeguridadRoutingModule } from './seguridad-routing.module';
 import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
 import { IdentificacionComponent } from './identificacion/identificacion.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { IdentificacionComponent } from './identificacion/identificacion.compone
   ],
   imports: [
     CommonModule,
-    SeguridadRoutingModule
+    SeguridadRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SeguridadModule { }
